6d9474849e37775ecefd4bd98491affb63f0bb41
Brian3647 <victoris3647@gmail.com>
2021-02-03 18:30:17 +0100

    ?

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require("path")
+const ips = require('./localip')();
+const port = process.env.PORT || 3000;
+
+console.log(ips);
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -8,6 +12,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "views/index.html"))
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(port, () => {
   console.log("Web on!")
 })
